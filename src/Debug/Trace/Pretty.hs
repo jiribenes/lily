3effee6d85c6f6c3328004dbe4703778a9c7aeb7
Jiří Beneš <mail@jiribenes.com>
2020-05-10 20:18:32 +0200

    Document Debug.Trace.Pretty

--- src/Debug/Trace/Pretty.hs
+++ src/Debug/Trace/Pretty.hs
@@ -10,11 +10,15 @@ import           Data.Text.Prettyprint.Doc      ( pretty
                                                 )
 import           Debug.Trace
 
+-- | Takes a 'Pretty' value, prints it and returns something else
 tracePretty :: Pretty a => a -> b -> b
 tracePretty x y = traceShow (pretty x) y
 
+-- | Takes a 'Pretty' value, prints it and returns it
 tracePrettyId :: Pretty a => a -> a
 tracePrettyId x = tracePretty x x
 
+-- | Takes a 'Pretty' value in a 'Applicative' context
+-- and prints it
 tracePrettyM :: (Pretty a, Applicative f) => a -> f ()
 tracePrettyM x = traceShowM (pretty x)
