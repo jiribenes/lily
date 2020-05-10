0ba92a7430de86e5cd7634381b8a73be510c15ea
Jiří Beneš <mail@jiribenes.com>
2020-05-10 20:25:08 +0200

    Modify examples, add ./.hie/ to gitignore

--- examples/recursive.cpp
+++ examples/recursive.cpp
@@ -1,26 +1,73 @@
-int factorial(int n) {
+int rec(int x) {
+    if (x == 1) {
+        return x;
+    }
+    return rec(x);
+}
+
+int main() {
+    return 1;
+}
+
+int add(int x, int y) {
+    int result = x + y;
+    return result;
+}
+
+void test(int x) {
+    int result = x * x;
+}
+
+int factorial2(int n) {
     if (n == 0) {
         return 1;
     }
     else {
-        return n * factorial(n-1);
+        return n * factorial2(n-1);
     }
 }
 
-int even(unsigned int n);
-int odd(unsigned int n);
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
 
-int odd(unsigned int n) {
+    return n * factorial(n-1);
+}
+
+int even(int n);
+int odd(int n);
+
+int odd(int n) {
     if (n == 0) return 0;
     else return even(n - 1);
 }
 
-int even(unsigned int n) {
+int even(int n) {
     if (n == 0) return 1;
-    else if (n == 100000) return odd(1);
+    //else if (n == 100000) return odd(1);
     else return odd(n - 1);
 }
 
-int square(unsigned int n) {
+int square(int n) {
     return n * n;
+}
+
+int uses_even(int n) {
+    return even(n) * 2;
+}
+
+template <typename T, typename U, typename Z>
+T konst(T a, U b) {
+    return a;
+}
+
+int fibbonacci(int n) {
+   if (n == 0){
+      return 0;
+   } else if (n == 1) {
+      return 1;
+   } else {
+      return fibbonacci(n-1) + fibbonacci(n-2);
+   }
 }
