3b344d1f3f72461214e424c048d78ef0d546cb9f
Jiří Beneš <mail@jiribenes.com>
2020-05-12 21:29:21 +0200

    Update clang-pure fork to 0.2.1.0

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -1,5 +1,5 @@
 builtins.fetchGit {
   url = "https://github.com/nixos/nixpkgs.git";
   ref = "refs/heads/nixos-unstable";
-  rev = "22a3bf9fb9edad917fb6cd1066d58b5e426ee975";
+  rev = "fce7562cf46727fdaf801b232116bc9ce0512049";
 }
