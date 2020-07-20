aaf1c3de0a889a79cd2cba47a2489bdcedd5d2a9
Jiří Beneš <mail@jiribenes.com>
2020-07-20 22:16:57 +0200

    Cleanup

--- src/Clang/Type.hs
+++ src/Clang/Type.hs
@@ -6,7 +6,9 @@ where
 
 import qualified Data.ByteString.Char8         as BS
 import           Data.Functor                   ( (<&>) )
-import           Debug.Trace                    (traceShowId,  traceShow )
+import           Debug.Trace                    ( traceShowId
+                                                , traceShow
+                                                )
 import           GHC.Stack                      ( HasCallStack )
 import           Language.C.Clang.Type          ( typeElementType
                                                 , typeArgTypes
@@ -19,8 +21,7 @@ import qualified Language.C.Clang.Type         as ClangType
 
 import           Type.Type
 import           Name                           ( nameFromBS )
-import           Control.Monad                  ( unless )
-import Data.Maybe (fromJust)
+import           Data.Maybe                     ( fromJust )
 
 data Ref = LRef | RRef | UnspecifiedRef
 
@@ -31,58 +32,62 @@ type ClangType = ClangType.Type
 -- which indicates an unrecoverable error in Clang itself.
 -- We'll just error in that case since what else is to do there?
 fromClangType :: HasCallStack => ClangType -> Maybe Type
-fromClangType clangType = case traceShowId $ ClangType.typeKind canonicalClangType of
-  ClangType.Invalid   -> error "internal error: invalid Clang type!"
-  ClangType.Unexposed -> traceShow
-    (  "Found type: "
-    <> BS.unpack (typeSpelling clangType)
-    <> ", canonically:"
-    <> BS.unpack (typeSpelling canonicalClangType)
-    <> ", but returning Nothing for now! TODO"
-    )
-    Nothing -- TODO: this generally means that the type is a bit involved and you should just use a new type constructor with the given spelling!
-  ClangType.Void   -> pure typeUnit
-  ClangType.Bool   -> pure typeBool
-  ClangType.Int    -> pure typeInt
-  ClangType.UInt   -> pure typeUInt
-  ClangType.Char_S -> pure typeChar
-  ClangType.Pointer -> do
-    let canonicalPointee = typeCanonicalType $ fromJust $ typePointeeType canonicalClangType
-    case ClangType.typeKind canonicalPointee of
-      -- special case for function pointers
-      ClangType.FunctionProto -> fromClangType canonicalPointee
-      _ -> fromClangType canonicalPointee <&> typePtrOf
-  ClangType.Record -> do
-    let name = nameFromBS $ typeSpelling canonicalClangType
-    let tc   = TC name StarKind -- TODO: get kind from clang
-    pure (TCon tc)
-  ClangType.LValueReference ->
-    typePointeeType canonicalClangType >>= fromClangType <&> MutRef
-  ClangType.FunctionProto -> do
-    clangArgTypes <- typeArgTypes canonicalClangType
-    resultType    <- fromClangType =<< typeResultType canonicalClangType
+fromClangType clangType =
+  case traceShowId $ ClangType.typeKind canonicalClangType of
+    ClangType.Invalid   -> error "internal error: invalid Clang type!"
+    ClangType.Unexposed -> traceShow
+      (  "Found type: "
+      <> BS.unpack (typeSpelling clangType)
+      <> ", canonically:"
+      <> BS.unpack (typeSpelling canonicalClangType)
+      <> ", but returning Nothing for now! TODO"
+      )
+      Nothing -- TODO: this generally means that the type is a bit involved and you should just use a new type constructor with the given spelling!
+    ClangType.Void    -> pure typeUnit
+    ClangType.Bool    -> pure typeBool
+    ClangType.Int     -> pure typeInt
+    ClangType.UInt    -> pure typeUInt
+    ClangType.Char_S  -> pure typeChar
+    ClangType.Pointer -> do
+      let canonicalPointee =
+            typeCanonicalType $ fromJust $ typePointeeType canonicalClangType
+      case ClangType.typeKind canonicalPointee of
+        -- special case for function pointers
+        ClangType.FunctionProto -> fromClangType canonicalPointee
+        _                       -> fromClangType canonicalPointee <&> typePtrOf
+    ClangType.Record -> do
+      let name = nameFromBS $ typeSpelling canonicalClangType
+      let tc   = TC name StarKind -- TODO: get kind from clang
+      pure (TCon tc)
+    ClangType.LValueReference ->
+      typePointeeType canonicalClangType >>= fromClangType <&> MutRef
+    ClangType.FunctionProto -> do
+      clangArgTypes <- typeArgTypes canonicalClangType
+      resultType    <- fromClangType =<< typeResultType canonicalClangType
 
-    argTypes      <- traverse getArgumentType clangArgTypes
+      argTypes      <- traverse getArgumentType clangArgTypes
 
-    pure $ foldr (\(ty, ref) acc -> (refToArrow ref) `TAp` ty `TAp` acc) resultType argTypes
-  ClangType.ConstantArray ->
-    typeElementType canonicalClangType >>= fromClangType <&> typeListOf
-  other -> traceShow
-    (unlines
-      [ "internal error: Encountered unknown Clang type (no conversion available!)"
-      , "\t\tName:\t\t\t" <> BS.unpack (typeSpelling clangType)
-      , "\t\tCanonical type name:\t"
-        <> BS.unpack (typeSpelling canonicalClangType)
-      , "\t\tKind:\t\t\t'" <> show other <> "'"
-      ]
-    )
-    Nothing
-  where canonicalClangType = typeCanonicalType clangType
+      pure $ foldr makeArrow resultType argTypes
+    ClangType.ConstantArray ->
+      typeElementType canonicalClangType >>= fromClangType <&> typeListOf
+    other -> traceShow
+      (unlines
+        [ "internal error: Encountered unknown Clang type (no conversion available!)"
+        , "\t\tName:\t\t\t" <> BS.unpack (typeSpelling clangType)
+        , "\t\tCanonical type name:\t"
+          <> BS.unpack (typeSpelling canonicalClangType)
+        , "\t\tKind:\t\t\t'" <> show other <> "'"
+        ]
+      )
+      Nothing
+ where
+  canonicalClangType = typeCanonicalType clangType
+  makeArrow (ty, ref) = \acc -> refToArrow ref `TAp` ty `TAp` acc
 
 refToArrow :: Ref -> Type
-refToArrow RRef = typeLinArrow
-refToArrow LRef = typeUnArrow
-refToArrow UnspecifiedRef = typeLinArrow
+refToArrow RRef           = typeLinArrow
+refToArrow LRef           = typeUnArrow
+refToArrow UnspecifiedRef = typeUnArrow
 
 getArgumentType :: HasCallStack => ClangType -> Maybe (Type, Ref)
 getArgumentType clangType = case ClangType.typeKind canonicalClangType of
@@ -93,8 +98,8 @@ getArgumentType clangType = case ClangType.typeKind canonicalClangType of
   ClangType.RValueReference -> do
     let maybePointee = typePointeeType canonicalClangType
     ty <- fromClangType =<< maybePointee
-    pure (ty, LRef) 
-  other -> do
+    pure (ty, LRef)
+  _ -> do
     ty <- fromClangType clangType
     pure (ty, UnspecifiedRef)
   where canonicalClangType = typeCanonicalType clangType
